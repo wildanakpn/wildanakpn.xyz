39e78a66ebb113c8b0ccfcba80a399bf0f94365f
Wildan Akbar Putra Nugraha <128437193+wildanakpn@users.noreply.github.com>
2024-06-11 11:35:17 +0700

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -221,17 +221,3 @@ self.addEventListener("fetch", (event) => {
 })();
 
 
-
-/*==================== Vercel Storage ====================*/
-import { NextResponse } from 'next/server';
-import { get } from '@vercel/edge-config';
-
-export const config = { matcher: '/welcome' };
-
-export async function middleware() {
-  const greeting = await get('greeting');
-  // NextResponse.json requires at least Next v13.1 or
-  // enabling experimental.allowMiddlewareResponseBody in next.config.js
-  return NextResponse.json(greeting);
-}
-/*==================== Vercel Storage ====================*/
